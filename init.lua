e9798c63c089dabd17c6b52db0e01a12b2b97f56
thangpham4 <thang.pham4@tiki.vn>
2023-06-13 23:02:04 +0700

    update

--- init.lua
+++ init.lua
@@ -131,14 +131,14 @@ require('lazy').setup({
   },
   {
     {
-        "kdheepak/lazygit.nvim",
-        dependencies =  {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim"
-        },
-        config = function()
-            require("telescope").load_extension("lazygit")
-        end,
+      "kdheepak/lazygit.nvim",
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim"
+      },
+      config = function()
+        require("telescope").load_extension("lazygit")
+      end,
     },
   },
   {
@@ -396,6 +396,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.cmd([[nnoremap <silent><Leader><C-]> <C-w><C-]><C-w>T]])
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
---
+++ lua/custom/plugins/airline.lua
@@ -0,0 +1,19 @@
+vim.cmd([[
+let g:airline_powerline_fonts = 1                       " Enable font for status bar
+let g:airline_solarized_bg='light'
+let g:airline_theme='solarized'
+let g:airline#extensions#tabline#enabled = 1            " Enable Tab bar
+let g:airline#extensions#tabline#left_sep = ' '         " Enable Tab seperator
+let g:airline#extensions#tabline#left_alt_sep = '|'     " Enable Tab seperator
+let g:airline#extensions#tabline#formatter = 'default'
+let g:airline#extensions#tabline#fnamemod = ':t'        " Set Tab name as file name
+let g:airline#extensions#tabline#tab_nr_type = 1 " tab number
+
+let g:airline#extensions#whitespace#enabled = 0         " Remove warning whitespace"
+
+let g:airline_section_error=''
+  ]])
+
+return {
+  'vim-airline/vim-airline',
+} 
